6aa46a9396561a063a648aef98590d540da9fb38
Felipe Ferreira <felizpeu@MacBook-Pro-de-vitor-2.local>
2023-01-05 01:23:05 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './global.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hello, Bruce!</h1>
+    </div>
+  )
+}
+
